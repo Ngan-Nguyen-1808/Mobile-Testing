373c29a8b2781fbdf2eb2c001c0ace3619aa7586
Ngan Thi Quynh Nguyen <ngantqnguyen@kms-technology.com>
2022-11-28 13:30:40 +0700

    Adding Test Suite Collection

---
+++ Mobile Demo/Test Suites/New Test Suite.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>New Test Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>ece8e0a5-781f-4958-8014-6825ac478a77</testSuiteGuid>
+   <testCaseLink>
+      <guid>30579eb6-2adf-4582-be3f-e1bfbba44683</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/New Test Case</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
